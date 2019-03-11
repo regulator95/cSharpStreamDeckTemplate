7c795a98a8cd2d27182e64cfced169af3df9b402
regulator95 <seinolf@gmail.com>
2019-03-11 12:40:33 -0400

    Initial Commit

---
+++ cSharpStreamDeckTemplate/Program.cs
@@ -0,0 +1,130 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using streamdeck_client_csharp;
+using streamdeck_client_csharp.Events;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        public class Options
+        {
+            [Option("port", Required = true, HelpText = "The websocket port to connect to", SetName = "port")]
+            public int Port { get; set; }
+
+            [Option("pluginUUID", Required = true, HelpText = "The UUID of the plugin")]
+            public string PluginUUID { get; set; }
+
+            [Option("registerEvent", Required = true, HelpText = "The event triggered when the plugin is registered?")]
+            public string RegisterEvent { get; set; }
+
+            [Option("info", Required = true, HelpText = "Extra JSON launch data")]
+            public string Info { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int count = 0; count < args.Length; count++)
+            {
+                if (args[count].StartsWith("-") && !args[count].StartsWith("--"))
+                {
+                    args[count] = $"-{args[count]}";
+                }
+            }
+
+            Parser parser = new Parser((with) =>
+            {
+                with.EnableDashDash = true;
+                with.CaseInsensitiveEnumValues = true;
+                with.CaseSensitive = false;
+                with.IgnoreUnknownArguments = true;
+                with.HelpWriter = Console.Error;
+            });
+
+            ParserResult<Options> options = parser.ParseArguments<Options>(args);
+            options.WithParsed<Options>(o => RunPlugin(o));
+        }
+
+        static void RunPlugin(Options options)
+        {
+            ManualResetEvent connectEvent = new ManualResetEvent(false);
+            ManualResetEvent disconnectEvent = new ManualResetEvent(false);
+
+            StreamDeckConnection connection = new StreamDeckConnection(options.Port, options.PluginUUID, options.RegisterEvent);
+
+            connection.OnConnected += (sender, args) =>
+            {
+                connectEvent.Set();
+            };
+
+            connection.OnDisconnected += (sender, args) =>
+            {
+                disconnectEvent.Set();
+            };
+
+            connection.OnApplicationDidLaunch += (sender, args) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"App Launch: {args.Event.Payload.Application}");
+            };
+
+            connection.OnApplicationDidTerminate += (sender, args) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"App Terminate: {args.Event.Payload.Application}");
+            };
+
+            Dictionary<string, JObject> settings = new Dictionary<string, JObject>();
+
+            connection.OnKeyUp += (sender, args) =>
+            {
+                JObject c_settings = args.Event.Payload.Settings;
+                Thread thread = new Thread(() => Clipboard.SetText(System.Net.WebUtility.UrlDecode(args.Event.Payload.Settings["textDemoValue"].ToString())));
+                thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+                thread.Start();
+                thread.Join();
+                System.Windows.Forms.SendKeys.SendWait("^(V)");
+
+            };
+
+
+            connection.OnDidReceiveSettings += (sender, args) =>
+            {
+            };
+
+            connection.OnWillDisappear += (sender, args) =>
+            {
+
+                lock (settings)
+                {
+                    if (settings.ContainsKey(args.Event.Context))
+                    {
+                        settings.Remove(args.Event.Context);
+                    }
+                }
+            };
+
+            // Start the connection
+            connection.Run();
+
+            // Wait for up to 10 seconds to connect
+            if (connectEvent.WaitOne(TimeSpan.FromSeconds(10)))
+            {
+                // We connected, loop every second until we disconnect
+                while (!disconnectEvent.WaitOne(TimeSpan.FromMilliseconds(1000)))
+                {
+                }
+            }
+        }
+    }
+}
